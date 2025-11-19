5ea6f92e7e2fc94b49ea456f53f17f2c3b093929
Aaron <aaronjbong@gmail.com>
2025-11-18 21:36:58 -0800

    Instructions to setup Data directory. Script to retrieve baseline model and save initial weights.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cs7643_model_quantization
 CS7643 Model Quantization Final Project
+
+#### Steps
+The datasets are not included in the repository to be compliant with license restrictions.
+To obtain the datasets:
+
+1. Register at https://www.cityscapes-dataset.com/.
+2. Download:
+   - `leftImg8Bit_trainvaltest.zip`
+   - `gtFine_trainvaltest.zip`
+3. Place them in `data/`.
+4. Unzip the files.
---
+++ src/models/deeplabv3_mnv3.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torchvision
+
+# We will use the DeepSeekV3 with MobileNetV3 backbone model.
+# # https://docs.pytorch.org/vision/main/_modules/torchvision/models/segmentation/deeplabv3.html#deeplabv3_mobilenet_v3_large
+
+# We will use the same taxonomy as the official CityScapes evaluation script. NUM_CLASSES = 19
+# https://github.com/mcordts/cityscapesScripts/blob/master/cityscapesscripts/helpers/labels.py
+NUM_CLASSES = 19
+
+def get_empty_model(num_classes = NUM_CLASSES):
+    model = torchvision.models.segmentation.deeplabv3_mobilenet_v3_large(
+        weights=None,
+        num_classes=num_classes,
+    )
+
+    # Replace classifier head
+    model.classifier[4] = nn.Conv2d(256, num_classes, kernel_size=(1, 1), stride=(1, 1))
+    return model
+
+def get_pretrained_model(num_classes = NUM_CLASSES):
+    model = torchvision.models.segmentation.deeplabv3_mobilenet_v3_large(
+        weights="COCO_WITH_VOC_LABELS_V1"
+    )
+
+    # Replace classifier head
+    model.classifier[4] = nn.Conv2d(256, num_classes, kernel_size=(1, 1), stride=(1, 1))
+    return model
+
+def save_model(model, path):
+    torch.save(model.state_dict(), path)
+
+def load_model(model, path, device="cpu"):
+    state = torch.load(path, map_location=device)
+    model.load_state_dict(state)
+    return model
