3faace5aaf4b5adf78c61b9c94415a222170f3ce
Aaron <aaronjbong@gmail.com>
2025-12-02 00:04:35 -0800

    Move to __main__.

--- pipeline/fine_tuning.py
+++ pipeline/fine_tuning.py
@@ -4,63 +4,65 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from src.models.deeplabv3_mnv3 import get_pretrained_model, save_model, load_model
+from src.models.deeplabv3_mnv3 import get_empty_model, save_model, load_model
 
 training_image_folder = "./data/leftImg8bit_trainvaltest/leftImg8bit/train"
 training_label_folder = "./data/gtFine_trainId/gtFine/train"
 validation_image_folder = "./data/leftImg8bit_trainvaltest/leftImg8bit/val"
 validation_label_folder = "./data/gtFine_trainId/gtFine/val"
 
-# Load configuration.
-with open("configs/ptq.yaml", "r") as f:
-    config = yaml.safe_load(f)["fp"]
+if __name__ == "__main__":
+    # Load configuration.
+    print("----- Running FP Training Script -----")
+    with open("configs/ptq.yaml", "r") as f:
+        config = yaml.safe_load(f)["fp"]
 
-batch_size = config['training']['batch_size']
+    batch_size = config['training']['batch_size']
 
-training_dataset = cityScapesDataset(training_image_folder, training_label_folder, config['training']['train_transforms'])
-validation_dataset = cityScapesDataset(validation_image_folder, validation_label_folder, config['training']['val_transforms'])
+    training_dataset = cityScapesDataset(training_image_folder, training_label_folder, config['training']['train_transforms'])
+    validation_dataset = cityScapesDataset(validation_image_folder, validation_label_folder, config['training']['val_transforms'])
 
-training_loader = DataLoader(training_dataset, batch_size, shuffle=True)
-validation_loader = DataLoader(validation_dataset, batch_size, shuffle=True)
+    training_loader = DataLoader(training_dataset, batch_size, shuffle=True)
+    validation_loader = DataLoader(validation_dataset, batch_size, shuffle=True)
 
-device = 'mps' if torch.backends.mps.is_available() else ('cuda' if torch.cuda.is_available() else 'cpu')
-print(f"Using Device: {device}")
-model = load_model(get_pretrained_model(19), "./models/baseline_init_model.pth", device=device)
-model = model.to(device)
+    device = 'mps' if torch.backends.mps.is_available() else ('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"Using Device: {device}")
+    model = load_model(get_empty_model(19), "./models/baseline_init_model.pth", device=device)
+    model = model.to(device)
 
-#Freeze all but the last 3 layers. Only optimize these
-for parameter in model.parameters():
-    parameter.requires_grad = False
+    #Freeze all but the last 3 layers. Only optimize these
+    for parameter in model.parameters():
+        parameter.requires_grad = False
 
-for name, param in model.named_parameters():
-    if 'classifier.2' in name or 'classifier.4' in name or 'aux_classifier.4' in name:
-        param.requires_grad = True
+    for name, param in model.named_parameters():
+        if 'classifier.2' in name or 'classifier.4' in name or 'aux_classifier.4' in name:
+            param.requires_grad = True
 
-loss_function = nn.CrossEntropyLoss(ignore_index=255)
-optimizer = optim.Adam([param for param in model.parameters() if param.requires_grad], lr=float(config['training']['learning_rate']),
-                                            weight_decay=float(config['training']['weight_decay']))
+    loss_function = nn.CrossEntropyLoss(ignore_index=255)
+    optimizer = optim.Adam([param for param in model.parameters() if param.requires_grad], lr=float(config['training']['learning_rate']),
+                                                weight_decay=float(config['training']['weight_decay']))
 
-epochs = config['training']['epochs']
-for epoch in range(epochs):
-    model.train()
-    training_loss = 0
-    for image, labels in training_loader:
-        optimizer.zero_grad()
-        out = model(image)['out']
-        loss = loss_function(out, labels)
-        training_loss += loss
-        loss.backward()
-        optimizer.step()
-    
-    model.eval()
-    validation_loss = 0
-    with torch.no_grad():
-        for image, labels in validation_loader:
+    epochs = config['training']['epochs']
+    for epoch in range(epochs):
+        model.train()
+        training_loss = 0
+        for image, labels in training_loader:
+            optimizer.zero_grad()
             out = model(image)['out']
             loss = loss_function(out, labels)
-            validation_loss += loss
-    
-    average_training_loss = training_loss / len(training_loader)
-    average_validation_loss = validation_loss / len(validation_loader)
-    print(f"Epoch: {epoch}, Training Loss: {average_training_loss}, Validation Loss: {average_validation_loss}")
-    
+            training_loss += loss
+            loss.backward()
+            optimizer.step()
+        
+        model.eval()
+        validation_loss = 0
+        with torch.no_grad():
+            for image, labels in validation_loader:
+                out = model(image)['out']
+                loss = loss_function(out, labels)
+                validation_loss += loss
+        
+        average_training_loss = training_loss / len(training_loader)
+        average_validation_loss = validation_loss / len(validation_loader)
+        print(f"Epoch: {epoch}, Training Loss: {average_training_loss}, Validation Loss: {average_validation_loss}")
+        
