7bcaf8d89fecae7d251b0aab9182c15c8845aca0
Naomi Martin <nmartin87@gatech.edu>
2025-12-08 21:07:10 -0800

    qat_debug

---
+++ pipeline/qat_debug.py
@@ -0,0 +1,282 @@
+import os
+import glob
+import yaml
+from PIL import Image
+from tqdm import tqdm
+
+import numpy as np
+import itertools
+import json
+import copy
+
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.ao.quantization as tq
+import torch.ao.quantization.quantize_fx as quantize_fx
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as T
+from torchvision.transforms import functional as F
+from pipeline.metrics import calculate_miou, calculate_model_size, measure_inference_latency
+
+from src.models.deeplabv3_mnv3 import get_empty_model, load_model
+from src.quantization.quantization_utils import set_seed, build_qconfig
+from pipeline.create_dataset import cityScapesDataset
+
+def plot_loss(train_losses, val_losses):
+    fig, ax = plt.subplots()
+    epochs = range(1, len(train_losses)+1)
+    ax.plot(epochs, train_losses, marker='o', label='train')
+    ax.plot(epochs, val_losses, marker='o', label='val')
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('Loss')
+    ax.legend(loc='upper right')
+    ax.set_title('Training and Validation Loss')
+    ax.grid()
+    return fig, ax
+
+def plot_miou(mious):
+    fig, ax = plt.subplots()
+    epochs = range(1, len(mious)+1)
+    ax.plot(epochs, mious, marker='o')
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('mIOU')
+    ax.set_title('Validation mIOU')
+    ax.grid()
+    return fig, ax
+
+def run_miou(model, device, dataloader):
+    # Run inference on full validation set and calculate mIoU
+    print(f"\nRunning inference on validation set...")
+    all_predictions = []
+    all_targets = []
+    
+    with torch.no_grad():
+        for i, (image, labels) in enumerate(tqdm(dataloader, desc="Validation inference")):
+            image = image.to(device, non_blocking=True)
+            
+            out = model(image)['out']
+            preds = out.argmax(dim=1)
+            
+            all_predictions.append(preds.cpu())
+            all_targets.append(labels)
+            
+    # Concatenate all predictions and targets
+    all_predictions = torch.cat(all_predictions, dim=0)
+    all_targets = torch.cat(all_targets, dim=0)
+    
+    # Calculate mIoU
+    print(f"\nCalculating mIoU...")
+    miou, per_class_ious = calculate_miou(all_predictions, all_targets, num_classes=19, ignore_index=255)
+    print(f"mIoU: {miou:.4f}")
+
+def run_qat(idx, config, results_dir):
+    set_seed()
+    print("--- Running QAT Script ---")
+
+    device = 'mps' if torch.backends.mps.is_available() else ('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"Using Device: {device}")
+
+    print(f"Loading Model from checkpoint: {config['model_checkpoint']} ...")
+    # Get empty model and load checkpoint weights.
+    model = get_empty_model()
+    model = load_model(model, config["model_checkpoint"], device=device)
+    model.eval()
+    print(f"Baseline Model Size (MB): {os.path.getsize(config['model_checkpoint']) / 1e6:.2f}")
+
+    # Get Quantization Configuration
+    print(f"Building QConfig with mode: {config['mode']}...")
+    qconfig_mapping = build_qconfig("qat", config)
+    print(f"qconfig_mapping global config: {qconfig_mapping.global_qconfig}")
+    print(f"qconfig_mapping weight config: {qconfig_mapping.global_qconfig.weight}")
+    print(f"qconfig_mapping activation config: {qconfig_mapping.global_qconfig.activation}")
+
+    train_img_path = "data/leftImg8bit_trainvaltest/leftImg8bit/train"
+    train_label_path = "data/gtFine_trainId/gtFine/train"
+    val_img_path = "data/leftImg8bit_trainvaltest/leftImg8bit/val"
+    val_label_path = "data/gtFine_trainId/gtFine/val"
+
+    cal_dataset = cityScapesDataset(val_img_path, val_label_path, config['training']['val_transforms'])
+    train_dataset = cityScapesDataset(train_img_path, train_label_path, config['training']['train_transforms'])
+    val_dataset = cityScapesDataset(val_img_path, val_label_path, config['training']['val_transforms'])
+
+    cal_dataloader = DataLoader(cal_dataset, batch_size=2, shuffle=False, num_workers=4, pin_memory=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=config['training']['batch_size'], shuffle=False, num_workers=4, pin_memory=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=config['training']['batch_size'], shuffle=False, num_workers=4, pin_memory=True)
+
+    example_image, _ = next(iter(cal_dataloader))
+    example_image = example_image.to(device)
+    prepared_model = quantize_fx.prepare_qat_fx(model, qconfig_mapping, (example_image,))
+    prepared_model = prepared_model.to(device)
+    prepared_model.eval()
+
+    loss_function = nn.CrossEntropyLoss(ignore_index=255)
+    optimizer = optim.Adam(prepared_model.parameters(), lr=float(config['training']['learning_rate']),
+                                                        weight_decay=float(config['training']['weight_decay']))
+    scheduler = CosineAnnealingLR(optimizer, T_max = config['training']['epochs'], eta_min = 1e-5)
+
+    # print("Calculate mIOU BEFORE calibration")
+    # run_miou(prepared_model, device, val_dataloader)
+
+    print("Start Calibration...")
+    if config['calibration']['enabled']:
+        with torch.no_grad():
+            for i, (image, _) in enumerate(cal_dataloader):
+                image = image.to(device, non_blocking=True)
+
+                prepared_model(image)
+                if (i % 10 == 0 and i > 0):
+                    print(f"  Calibrated {i} batches ...")
+
+                if (i >= config['calibration']['steps'] - 1):
+                    print(f"  Completed {config['calibration']['steps']} calibration steps.")
+                    break
+
+    # print("Calculate mIOU AFTER calibration")
+    # run_miou(prepared_model, device, val_dataloader)
+
+    print("Starting QAT...")
+    train_losses = []
+    val_losses = []
+    val_mious = []
+
+    epochs = config['training']['epochs']
+    for epoch in range(epochs):
+        prepared_model.train()
+        training_loss = 0
+        for image, label in tqdm(train_dataloader, desc=f"Training Epoch {epoch}"):
+            image = image.to(device, non_blocking=True)
+            label = label.to(device, non_blocking=True)
+
+            optimizer.zero_grad()
+            out = prepared_model(image)['out']
+            loss = loss_function(out, label)
+            training_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+
+        prepared_model.eval()
+        validation_loss = 0
+        all_predictions = []
+        all_targets = []
+        with torch.no_grad():
+            for image, label in tqdm(val_dataloader, desc=f"Validation Epoch {epoch}"):
+                image = image.to(device, non_blocking=True)
+                label = label.to(device, non_blocking=True)
+
+                out = prepared_model(image)['out']
+                pred = out.argmax(dim=1)
+                loss = loss_function(out, label)
+                validation_loss += loss.item()
+
+                all_predictions.append(pred.cpu())
+                all_targets.append(label)
+        
+        scheduler.step()
+
+        average_training_loss = training_loss / len(train_dataloader)
+        average_validation_loss = validation_loss / len(val_dataloader)
+
+        train_losses.append(average_training_loss)
+        val_losses.append(average_validation_loss)
+
+        print(f"Epoch: {epoch}, Training Loss: {average_training_loss}, Validation Loss: {average_validation_loss}")
+
+        print(f"Calculate mIOU DURING training epoch {epoch}")
+        # Concatenate all predictions and targets
+        all_predictions = torch.cat(all_predictions, dim=0)
+        all_targets = torch.cat(all_targets, dim=0)
+        # Calculate mIoU
+        print(f"\nCalculating mIoU...")
+        miou, per_class_ious = calculate_miou(all_predictions.cpu(), all_targets.cpu(), num_classes=19, ignore_index=255)
+        print(f"mIoU: {miou:.4f}")
+        val_mious.append(miou)
+
+    print("Convert QAT model ...")
+    # must move model to CPU to convert, else it errors!
+    quantized_model = quantize_fx.convert_fx(prepared_model.to("cpu").eval())
+
+    print("Saving QAT Model ...")
+    model_path = os.path.join(results_dir, f"qat_quantized_model_{idx}.pth")
+    torch.save(quantized_model.state_dict(), model_path)
+    print(f"QAT Model Size (MB): {os.path.getsize(model_path) / 1e6:.2f}")
+  
+    print("Calculate mIOU on prepared model, device = cpu")
+    run_miou(prepared_model.to("cpu").eval(), "cpu", val_dataloader)
+
+    # print("eval miou w aarons function - prepared")
+    # evaluation = evaluate_model(
+    #     model = prepared_model.to("cpu").eval(),
+    #     val_image_folder = val_img_path,
+    #     val_label_folder = val_label_path,
+    #     device = "cpu",
+    #     batch_size = 1,
+    #     quantization_bits = 4
+    # )
+
+    print(f"Calculate mIOU on quantized model, device = {device}")
+    run_miou(quantized_model.to(device).eval, device, val_dataloader)
+    print("Calculate mIOU on quantized model, device = cpu")
+    run_miou(quantized_model.to("cpu").eval(), "cpu", val_dataloader)
+  
+    # Metrics
+    # print("eval miou w aarons function - quant")
+    # evaluation = evaluate_model(
+    #     model = quantized_model.to("cpu").eval(),
+    #     val_image_folder = val_img_path,
+    #     val_label_folder = val_label_path,
+    #     device = "cpu",
+    #     batch_size = 1,
+    #     quantization_bits = 4
+    # )
+
+
+    # save all results
+    result = {
+      "idx": idx,
+      "config": config,
+      "train_losses": [round(loss, 3) for loss in train_losses],
+      "val_losses": [round(loss, 3) for loss in val_losses],
+      "val_mious": [round(miou, 3) for miou in val_mious],
+      "final_train_loss": train_losses[-1],
+      "final_val_loss": val_losses[-1],
+      "final_val_miou": val_mious[-1]
+    }
+
+    json_path = os.path.join(results_dir, "qat_results.json")
+    if os.path.exists(json_path):
+      with open(json_path, "r") as f:
+        results = json.load(f)
+    else:
+      results = []
+
+    results.append(result)
+
+    with open(json_path, "w") as f:
+      json.dump(results, f, indent=2)
+
+    # save all results
+    fig, ax = plot_loss(train_losses, val_losses)
+    plot_path = os.path.join(results_dir, f"qat_loss_{idx}.png")
+    fig.savefig(plot_path)
+
+    fig, ax = plot_miou(val_mious)
+    plot_path = os.path.join(results_dir, f"miou_{idx}.png")
+    fig.savefig(plot_path)
+
+    del prepared_model
+    del quantized_model
+    torch.cuda.empty_cache()
+
+if __name__ == "__main__":
+    results_dir = "qat_results"
+    os.makedirs(results_dir, exist_ok=True)
+
+    # Load configuration.
+    print("Loading Configuration ...")
+    with open("configs/ptq.yaml", "r") as f:
+        config = yaml.safe_load(f)["qat"]
+
+    run_qat(0, config, results_dir)
